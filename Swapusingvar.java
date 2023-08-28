9b7dffb45238af673723ac7cf953190ead9311ce
Gurasis Singh Bamrah <61875021+ashurocks@users.noreply.github.com>
2023-08-28 15:27:13 +0530

    Add files via upload

---
+++ Swapusingvar.java
@@ -0,0 +1,15 @@
+class Swapnoswithvar{
+ public static void main(String[] args) {
+   int num1 = 40;
+   int num2 = 30;
+
+  int temp = num1;
+  num1 = num2;
+  num2 = temp;
+
+System.out.println("After swapping:");
+System.out.println("num1= " + num1);
+System.out.println("num2= " + num2);
+
+}
+};
