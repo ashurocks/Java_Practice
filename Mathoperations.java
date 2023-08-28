445823c7d76169741be97c749e14667a2c8c65aa
Gurasis Singh Bamrah <61875021+ashurocks@users.noreply.github.com>
2023-08-28 15:26:52 +0530

    Add files via upload

---
+++ Mathoperations.java
@@ -0,0 +1,21 @@
+public class Mathoperations {
+
+public static void main(String args[]) {
+  int num1 = 50;
+  int num2 = 10;
+
+  int sum = num1 + num2;
+  int product = num1 * num2;
+  int difference = num1 - num2;
+  int division = num1 / num2;  
+  int remainder = num1 % num2;
+
+  System.out.println("Sum= " + sum);
+  System.out.println("Product= " + product);
+  System.out.println("Difference= " + difference);
+  System.out.println("Division= " + division);
+  System.out.println("Remainder= " + remainder);
+
+}
+
+};
