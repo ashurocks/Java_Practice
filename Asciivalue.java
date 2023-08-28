aefb870cbb37c2d0b7b34afb8c73172f5386b53c
Gurasis Singh Bamrah <61875021+ashurocks@users.noreply.github.com>
2023-08-28 15:28:34 +0530

    Add files via upload

---
+++ Asciivalue.java
@@ -0,0 +1,11 @@
+class Ascii{
+  public static void main(String[] args) {
+    char character = 'G';
+
+    int ascii = (int) character;
+
+    System.out.println("The Value is: " + ascii);
+
+}
+
+};
